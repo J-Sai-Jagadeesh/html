73180c8693d96d77ebec3b123c47c9e0318ee720
J-Sai-Jagadeesh <jagadeeshjsai@gmail.com>
2025-06-19 12:34:28 +0530

    Add files via upload

---
+++ Loops/Equalandnotequal.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+class Equal{
+public static void main(String[] fjd)
+{
+Scanner s=new Scanner(System.in);
+System.out.println("Enter the Number a:");
+int a=s.nextInt();
+System.out.println("Enter the Number b:");
+int b=s.nextInt();
+if(a==b)
+{
+System.out.println("both are equal");
+}
+else
+{
+System.out.println("both r not equal");
+}
+}
+}
